96b57b8efa9835815dc0d0b49143d5f6625c203b
Rafau <rafal.filipek@gmail.com>
2023-03-03 22:07:47 +0000

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import * as React from "react";
 import "./styles.css";
-import { clsx } from "clsx";
-import { Transition } from "@headlessui/react";
 import { useWindowSize } from "./useWindowSize";
 import { useValue } from "./useValue";
-import { Item } from './Item'
+import { Item } from "./Item";
 
 export default function App() {
   const { set, value } = useValue(undefined);
@@ -19,90 +16,36 @@ export default function App() {
         "--menu-window-height": `calc(${height}px - 8rem)`,
       }}
     >
-      <div className="h-32 bg-blue-500 w-full flex border">
-        <Item isActive={value === 1} set={set} />
-        <div>
-          <button
-            className="p-3 text-xl"
-            onMouseEnter={() => set(2, 150)}
-            onClick={() => set(2)}
-            onKeyDown={(e) => {
-              e.key === "Escape" && set(undefined);
-            }}
-          >
-            <strong>Sklep</strong>
-          </button>
-          <Transition
-            show={value === 2}
-            as={React.Fragment}
-            enter="transition duration-300"
-            leave="transition duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div
-              className={clsx(
-                "bg-pink-500 absolute w-full left-0 top-0 text-6xl font-bold  p-10",
-                "flex flex-col overflow-auto lg:max-h-[var(--menu-window-height)]",
-                {
-                  "translate-y-32 --z-10": value === 2,
-                  "translate-y-32 -z-20": value !== 2,
-                }
-              )}
-            >
-              Sklep menu
-              <hr />
-              <div className="flex gap-10">
-                <a href="">001</a>
-                <a href="">002</a>
-                <a href="">003</a>
-              </div>
-            </div>
-          </Transition>
-        </div>
-        <div>
-          <button
-            className="p-3 text-xl"
-            onMouseEnter={() => set(3, 150)}
-            onClick={() => set(3)}
-            onKeyDown={(e) => {
-              e.key === "Escape" && set(undefined);
-            }}
-          >
-            <strong>Kontakt</strong>
-          </button>
-          <Transition
-            show={value === 3}
-            as={React.Fragment}
-            enter="transition duration-300"
-            leave="transition duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div
-              className={clsx(
-                "bg-green-500 absolute w-full left-0 top-0 text-6xl font-bold p-10",
-                "flex orange-col overflow-auto lg:max-h-[var(--menu-window-height)]",
-                {
-                  "translate-y-32 --z-10": value === 3,
-                  "translate-y-32 -z-20": value !== 3,
-                }
-              )}
-            >
-              Kontakt menu
-              <hr />
-              <div className="flex gap-10">
-                <a href="">001</a>
-                <a href="">002</a>
-                <a href="">003</a>
-              </div>
-            </div>
-          </Transition>
-        </div>
+      <div className="py-5 h-24 bg-blue-500 w-full flex">
+        <Item
+          id={1}
+          isActive={value === 1}
+          set={set}
+          title="Oferta"
+          ref={(ref) => {
+            console.log(ref);
+          }}
+        >
+          <div className="flex gap-5">
+            <a href="#">001</a>
+            <a href="#">002</a>
+            <a href="#">003</a>
+          </div>
+        </Item>
+        <Item id={2} isActive={value === 2} set={set} title="Sklep">
+          <div className="flex gap-5">
+            <a href="#">004</a>
+            <a href="#">005</a>
+            <a href="#">006</a>
+          </div>
+        </Item>
+        <Item id={3} isActive={value === 3} set={set} title="Kontat">
+          <div className="flex gap-5">
+            <a href="#">007</a>
+            <a href="#">008</a>
+            <a href="#">009</a>
+          </div>
+        </Item>
       </div>
     </div>
   );
--- src/Item.tsx
+++ src/Item.tsx
@@ -1,71 +1,73 @@
-import * as React from 'react';
+import * as React from "react";
 import { Transition } from "@headlessui/react";
-import clsx from 'clsx';
+import clsx from "clsx";
 
 interface IItemProps {
-  set: (id?: number, delay?: number) => void,
+  id: number;
+  set: (id?: number, delay?: number) => void;
   isActive: boolean;
+  title: string;
 }
 
-export function Item({ set, isActive }: IItemProps) {
+export const Item = React.memo(
+  React.forwardRef<HTMLButtonElement, React.PropsWithChildren<IItemProps>>(
+    ItemComponent
+  )
+);
+
+function ItemComponent(
+  { id, set, isActive, title, children }: React.PropsWithChildren<IItemProps>,
+  forwardRef: React.RefAttributes<HTMLButtonElement>["ref"]
+) {
+  const ref = React.useRef<HTMLDivElement>(null);
+  const beforeLeave = React.useCallback(() => {
+    if (ref.current) {
+      ref.current.querySelectorAll("a").forEach((el) => {
+        el.setAttribute("tabindex", "-1");
+      });
+    }
+  }, []);
+
+  React.useEffect(() => {});
+
   return (
-   <div>
-          <button
-            className="p-3 text-xl"
-            onMouseEnter={() => set(1, 150)}
-            onClick={() => set(1)}
-            onKeyDown={(e) => {
-              e.key === "Escape" && set(undefined);
-            }}
-          >
-            <strong>Oferta</strong>
-          </button>
-          <Transition
-            show={isActive}
-            as={React.Fragment}
-            enter="transition duration-300"
-            leave="transition duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div
-              className={clsx(
-                "bg-green-500 absolute w-full left-0 top-0 text-6xl font-bold  p-10 p-10",
-                "flex flex-col overflow-auto lg:max-h-[var(--menu-window-height)]",
-                {
-                  "translate-y-32 --z-10": isActive,
-                  "translate-y-32 -z-20": !isActive,
-                }
-              )}
-            >
-              Oferta menu
-              <hr />
-              <div className="flex gap-10">
-                <a href="">001</a>
-                <a href="">002</a>
-                <a href="">003</a>
-              </div>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Voluptas officiis recusandae, ex ut totam quam, impedit
-                distinctio repellendus provident quisquam voluptatum saepe
-                aspernatur dolorem Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Voluptas officiis recusandae, ex ut totam
-                quam, impedit distinctio repellendus provident quisquam
-                voluptatum saepe aspernatur dolorem Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Voluptas officiis recusandae, ex
-                ut totam quam, impedit distinctio repellendus provident quisquam
-                voluptatum saepe aspernatur dolorem Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Voluptas officiis recusandae, ex
-                ut totam quam, impedit distinctio repellendus provident quisquam
-                voluptatum saepe aspernatur dolorem Lorem ipsum dolor sit, amet
-                consectetur adipisicing elit. Voluptas officiis recusandae, ex
-                ut totam quam, impedit distinctio repellendus provident quisquam
-                voluptatum saepe aspernatur dolorem
-              </p>
-            </div>
-          </Transition>
-        </div>)
+    <div>
+      <button
+        ref={forwardRef}
+        className="p-3 text-xl"
+        onMouseEnter={() => set(id, 150)}
+        onClick={() => set(id)}
+        onKeyDown={(e) => {
+          e.key === "Escape" && set(undefined);
+        }}
+      >
+        <strong>{title}</strong>
+      </button>
+      <Transition
+        show={isActive}
+        as={React.Fragment}
+        enter="transition duration-300"
+        leave="transition duration-500"
+        beforeLeave={beforeLeave}
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div
+          ref={ref}
+          className={clsx(
+            "bg-green-500 absolute w-full left-0 top-0 text-6xl font-bold  p-10 p-10",
+            "flex flex-col overflow-auto lg:max-h-[var(--menu-window-height)]",
+            {
+              "translate-y-24 --z-10": isActive,
+              "translate-y-24 -z-20": !isActive,
+            }
+          )}
+        >
+          {children}
+        </div>
+      </Transition>
+    </div>
+  );
 }
